83e458136ee1e456610ebc18e4f2ed003f8bdc99
Sakshi Shrivastava <sak.20.shrivastava@gmail.com>
2018-11-24 16:06:51 -0800

    Updated with random selection

--- spl.py
+++ spl.py
@@ -33,7 +33,7 @@
 
 
 import xlrd, json
-from random import randint
+import random
 
 class dataCollection:
 
@@ -48,47 +48,53 @@ class dataCollection:
 		if questionType == 1:
 
 			sh1 = self.book.sheet_by_index(0)
-			rand = randint(1, sh1.nrows-15)
+			rand = random.sample(range(1,sh1.nrows), 15)
+			print(rand)
 
-			for rx in range(rand,rand+15):
-				question = {"Question": sh1.row(rx)[0].value,
-				"option1": sh1.row(rx)[1].value,
-				"option2": sh1.row(rx)[2].value,
-				"option3": sh1.row(rx)[3].value,
-				"option4": sh1.row(rx)[4].value,
-				"correctAnswer": sh1.row(rx)[5].value}
+			for rx in rand:
+				question = {"Question": sh1.row(rx)[1].value,
+				"option1": sh1.row(rx)[2].value,
+				"option2": sh1.row(rx)[3].value,
+				"option3": sh1.row(rx)[4].value,
+				"option4": sh1.row(rx)[5].value,
+				"correctAnswer": sh1.row(rx)[6].value}
 				self.store.append(question)	
 			return self.store
 
 		if questionType == 2:
 
 			sh2 = self.book.sheet_by_index(1)
-			rand = randint(1, sh2.nrows-15)
+			rand = random.sample(range(1,sh2.nrows), 15)
 
-			for rx in range(rand,rand+15):
-				question = {"Question": sh2.row(rx)[0].value,
-				"option1": sh2.row(rx)[1].value,
-				"option2": sh2.row(rx)[2].value,
-				"option3": sh2.row(rx)[3].value,
-				"option4": sh2.row(rx)[4].value,
-				"correctAnswer": sh2.row(rx)[5].value}
+			for rx in rand:
+				question = {"Question": sh2.row(rx)[1].value,
+				"option1": sh2.row(rx)[2].value,
+				"option2": sh2.row(rx)[3].value,
+				"option3": sh2.row(rx)[4].value,
+				"option4": sh2.row(rx)[5].value,
+				"correctAnswer": sh2.row(rx)[6].value}
 				self.store.append(question)
 			return self.store
 
 		if questionType == 3:
 
 			sh3 = self.book.sheet_by_index(2)
-			rand = randint(1, sh3.nrows-15)
+			rand = random.sample(range(1,sh3.nrows), 15)
 
-			for rx in range(rand,rand+15):
-				question = {"Question": sh3.row(rx)[0].value,
-				"option1": sh3.row(rx)[1].value,
-				"option2": sh3.row(rx)[2].value,
-				"option3": sh3.row(rx)[3].value,
-				"option4": sh3.row(rx)[4].value,
-				"correctAnswer": sh3.row(rx)[5].value}
+			for rx in rand:
+				question = {"Question": sh3.row(rx)[1].value,
+				"option1": sh3.row(rx)[2].value,
+				"option2": sh3.row(rx)[3].value,
+				"option3": sh3.row(rx)[4].value,
+				"option4": sh3.row(rx)[5].value,
+				"correctAnswer": sh3.row(rx)[6].value}
 				self.store.append(question)
 			return self.store
+
+obj = dataCollection('Spl_1.xlsx')
+ques = obj.getQuestions(3)
+for q in ques:
+	print(q)
 
 
 
